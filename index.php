348c2ba9ccf6894a4536bb98d7382f8a8293b639
Rochelle Lewis <rlewis37@cnm.edu>
2017-08-07 13:57:14 -0600

    created base grid column structure

--- index.php
+++ index.php
@@ -27,6 +27,101 @@
 		<title>Bootstrap Challenge</title>
 	</head>
 	<body>
-		boo!
+
+		<header>
+			<div class="container">
+				<nav class="navbar navbar-default">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Title</a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#">Link 1</a></li>
+							<li><a href="#">Link 2</a></li>
+							<li><a href="#">Link 3</a></li>
+							<li><a href="#">Link 4</a></li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</nav>
+			</div>
+		</header>
+
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="jumbotron">
+							<h1>Hello, World!</h1>
+							<p>Hammock slow-carb hell of waistcoat umami adaptogen butcher salvia cronut irony raw denim la croix kale chips.</p>
+							<button class="btn btn-primary">Click Me!</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<img class="img-responsive" src="https://www.fillmurray.com/300/300" alt="1980s Bill Murray">
+					</div>
+					<div class="col-md-9">
+						<p>Hammock slow-carb hell of waistcoat umami adaptogen butcher salvia cronut irony raw denim la croix kale chips. Snackwave fashion axe trust fund retro, cornhole blue bottle hashtag +1 offal kitsch tattooed XOXO biodiesel. Hella food truck kogi, bushwick photo booth fam plaid offal. Stumptown succulents yuccie hashtag dreamcatcher deep v occupy. Everyday carry semiotics photo booth seitan, swag mixtape prism heirloom beard knausgaard polaroid tofu neutra kickstarter. Pabst squid brunch, slow-carb banjo paleo biodiesel hammock pug. Franzen heirloom put a bird on it pug direct trade. Authentic crucifix knausgaard poutine copper mug tbh literally pitchfork.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<img class="img-responsive img-circle" src="https://www.fillmurray.com/250/250" alt="classy bill murray">
+						<div class="well">
+							<p>Cliche hoodie organic letterpress farm-to-table keffiyeh post-ironic shoreditch man bun. Twee cronut scenester, blog semiotics shabby chic selfies beard viral shoreditch.</p>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<img class="img-responsive img-circle" src="https://www.fillmurray.com/250/250" alt="classy bill murray">
+						<div class="well">
+							<p>Cliche hoodie organic letterpress farm-to-table keffiyeh post-ironic shoreditch man bun. Twee cronut scenester, blog semiotics shabby chic selfies beard viral shoreditch.</p>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<img class="img-responsive img-circle" src="https://www.fillmurray.com/250/250" alt="classy bill murray">
+						<div class="well">
+							<p>Cliche hoodie organic letterpress farm-to-table keffiyeh post-ironic shoreditch man bun. Twee cronut scenester, blog semiotics shabby chic selfies beard viral shoreditch.</p>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<img class="img-responsive img-circle" src="https://www.fillmurray.com/250/250" alt="classy bill murray">
+						<div class="well">
+							<p>Cliche hoodie organic letterpress farm-to-table keffiyeh post-ironic shoreditch man bun. Twee cronut scenester, blog semiotics shabby chic selfies beard viral shoreditch.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<small>Direct trade street art pitchfork vegan.</small>
+					</div>
+				</div>
+			</div>
+		</footer>
+
 	</body>
 </html>
